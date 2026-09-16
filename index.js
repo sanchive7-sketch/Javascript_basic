10573a2d71b5f8753cb89546f144c7d209ab18b0
Sri Sanchive Kumar <sanchive7@gmail.com>
2026-09-16 14:51:57 +0530

    Add calculator functionality with basic arithmetic operations

---
+++ index.js
@@ -0,0 +1,25 @@
+function calculate(event){
+    var num1 = parseFloat(document.getElementById("numberA").value);
+    var num2 = parseFloat(document.getElementById("numberB").value);
+    var operation = event.target.value;
+    var result;
+
+    switch(operation) {
+        case "Add":
+            result = num1 + num2;
+            break;
+        case "Subtract":
+            result = num1 - num2;
+            break;
+        case "Multiply":
+            result = num1 * num2;
+            break;
+        case "Divide":
+            result = num1 / num2;
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
